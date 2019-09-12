ee3b9eaa665bf242bbbfc24247413fdf91f3733c
rociac <brsrkdev@gmail.com>
2019-09-12 17:04:30 -0500

    Create edit option for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def new
     @user = User.new
   end
 
   def create
-
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])  #form tags
-   @user = User.new(user_params) #form_for
+    @user = User.new(user_params) # form_for
     if @user.save
       redirect_to new_user_path
     else
@@ -15,16 +16,16 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id]) 
+    @user = User.find(params[:id])
   end
 
   def edit
     @user = User.find(params[:id])
-    
   end
 
   def update
- 
+    @user = User.find(params[:id])
+    @user.update(user_params)
   end
 
   private
@@ -33,5 +34,3 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
 end
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'users/new'
   get 'users/create'
+  put 'users/:id', to: 'users#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :users 
+  resources :users
 end
